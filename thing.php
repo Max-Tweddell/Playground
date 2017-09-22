4dea3c5d8eb17c906bccdd0acb06096ed5d72393
Max Tweddell <maxt.nz@gmail.com>
2017-09-22 14:42:22 +1200

    added php thing

---
+++ thing.php
@@ -0,0 +1,21 @@
+
+<?php
+
+echo "hello world \n";
+
+echo 4 ** 4 ** 4 ** 4;
+
+
+echo "this is pi \n";
+
+function pifn($n) {
+    $sum = 0;
+    $sign = 1;
+    for ($i = 0; $i < $n; $i++) {
+        $sum += $sign / (2.0*$i+1.0);
+        $sign *= -1;
+    };
+    return (4.0 * $sum);
+}
+echo pifn(2000);
+?>
